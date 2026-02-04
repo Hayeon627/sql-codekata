4c7ca503c00a68f236d39792f5bcef422d50a445
Hayeon627 <hy2000627@gmail.com>
2026-02-04 12:15:35 +0900

    [LeetCode] 중급 - 196. Delete Duplicate Emails (#122)

--- leetcode/3_중급/122_196._Delete_Duplicate_Emails.sql
+++ leetcode/3_중급/122_196._Delete_Duplicate_Emails.sql
@@ -2,7 +2,7 @@
 -- LeetCode 중급 (⭐⭐⭐)
 -- 문제 링크: https://leetcode.com/problems/delete-duplicate-emails/
 -- 작성자: 김하연
--- 작성일: 2026. 02. 04. 12:14:58
+-- 작성일: 2026. 02. 04. 12:15:35
 
 # Write your MySQL query statement below
 DELETE p1 
