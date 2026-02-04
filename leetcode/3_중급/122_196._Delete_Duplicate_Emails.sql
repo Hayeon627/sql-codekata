b11e1634a0a7a1ebaa6626e1056704b500a5bdda
Hayeon627 <hy2000627@gmail.com>
2026-02-04 12:14:58 +0900

    [LeetCode] 중급 - 196. Delete Duplicate Emails (#122)

---
+++ leetcode/3_중급/122_196._Delete_Duplicate_Emails.sql
@@ -0,0 +1,11 @@
+-- 196. Delete Duplicate Emails
+-- LeetCode 중급 (⭐⭐⭐)
+-- 문제 링크: https://leetcode.com/problems/delete-duplicate-emails/
+-- 작성자: 김하연
+-- 작성일: 2026. 02. 04. 12:14:58
+
+# Write your MySQL query statement below
+DELETE p1 
+FROM Person p1, Person p2
+WHERE p1.Email = p2.Email 
+  AND p1.Id > p2.Id;
