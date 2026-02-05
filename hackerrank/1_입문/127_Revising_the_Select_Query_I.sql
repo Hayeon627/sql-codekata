689b9a5fc9b4d70e20727bbd8afc231b56cb2f79
Hayeon627 <hy2000627@gmail.com>
2026-02-05 15:21:27 +0900

    [HackerRank] 입문 - Revising the Select Query I (#127)

---
+++ hackerrank/1_입문/127_Revising_the_Select_Query_I.sql
@@ -0,0 +1,10 @@
+-- Revising the Select Query I
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/revising-the-select-query/problem
+-- 작성자: 김하연
+-- 작성일: 2026. 02. 05. 15:21:26
+
+SELECT *
+FROM CITY
+WHERE POPULATION > 100000
+AND COUNTRYCODE = 'USA'
