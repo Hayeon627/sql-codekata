65301ec8be42cef5fa24a566e19a381ff2a076d2
Hayeon627 <hy2000627@gmail.com>
2026-02-09 09:36:08 +0900

    [HackerRank] 입문 - Select By ID (#130)

---
+++ hackerrank/1_입문/130_Select_By_ID.sql
@@ -0,0 +1,9 @@
+-- Select By ID
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/select-by-id/problem
+-- 작성자: 김하연
+-- 작성일: 2026. 02. 09. 09:36:07
+
+SELECT *
+FROM CITY
+WHERE ID = 1661
