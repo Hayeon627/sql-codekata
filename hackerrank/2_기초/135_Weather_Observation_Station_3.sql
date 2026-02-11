623264dadad0280637fd1880403ffbe4448f9915
Hayeon627 <hy2000627@gmail.com>
2026-02-11 09:33:37 +0900

    [HackerRank] 기초 - Weather Observation Station 3 (#135)

---
+++ hackerrank/2_기초/135_Weather_Observation_Station_3.sql
@@ -0,0 +1,9 @@
+-- Weather Observation Station 3
+-- HackerRank 기초 (⭐⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/weather-observation-station-3/problem
+-- 작성자: 김하연
+-- 작성일: 2026. 02. 11. 09:33:36
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE ID % 2 = 0
