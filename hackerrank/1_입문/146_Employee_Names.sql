9528abe5e2192c1c383f74e9f6b3c7c130404d74
Hayeon627 <hy2000627@gmail.com>
2026-02-20 09:19:43 +0900

    [HackerRank] 입문 - Employee Names (#146)

---
+++ hackerrank/1_입문/146_Employee_Names.sql
@@ -0,0 +1,9 @@
+-- Employee Names
+-- HackerRank 입문 (⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/name-of-employees/problem
+-- 작성자: 김하연
+-- 작성일: 2026. 02. 20. 09:19:43
+
+SELECT NAME
+FROM EMPLOYEE
+ORDER BY NAME
