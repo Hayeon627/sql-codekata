67368a58473759d01e1ceddb49be3ed5ad825bcc
Hayeon627 <hy2000627@gmail.com>
2026-07-28 03:53:00 +0900

    [프로그래머스] 입문 - 루시와 엘라 찾기 (#40)

--- programmers/1_입문/40_루시와_엘라_찾기.sql
+++ programmers/1_입문/40_루시와_엘라_찾기.sql
@@ -2,7 +2,7 @@
 -- 프로그래머스 입문 (⭐)
 -- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59046
 -- 작성자: 김하연
--- 작성일: 2026. 07. 28. 03:52:43
+-- 작성일: 2026. 07. 28. 03:52:55
 
 -- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
