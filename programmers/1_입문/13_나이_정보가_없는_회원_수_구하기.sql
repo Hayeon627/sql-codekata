a6d3f2ee181e9143e60f379409c623963647f909
Hayeon627 <hy2000627@gmail.com>
2026-07-28 03:48:41 +0900

    [프로그래머스] 입문 - 나이 정보가 없는 회원 수 구하기 (#13)

--- programmers/1_입문/13_나이_정보가_없는_회원_수_구하기.sql
+++ programmers/1_입문/13_나이_정보가_없는_회원_수_구하기.sql
@@ -2,7 +2,7 @@
 -- 프로그래머스 입문 (⭐)
 -- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131528
 -- 작성자: 김하연
--- 작성일: 2026. 07. 28. 03:48:19
+-- 작성일: 2026. 07. 28. 03:48:36
 
 -- 코드를 입력하세요
 SELECT COUNT(*) AS USERS
