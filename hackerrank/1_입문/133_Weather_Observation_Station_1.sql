ecf55da7793446bde4097f705151af9e6f34a494
Hayeon627 <hy2000627@gmail.com>
2026-07-28 03:18:22 +0900

    [HackerRank] 입문 - Weather Observation Station 1 (#133)

--- hackerrank/1_입문/133_Weather_Observation_Station_1.sql
+++ hackerrank/1_입문/133_Weather_Observation_Station_1.sql
@@ -2,7 +2,7 @@
 -- HackerRank 입문 (⭐)
 -- 문제 링크: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
 -- 작성자: 김하연
--- 작성일: 2026. 02. 10. 09:20:28
+-- 작성일: 2026. 07. 28. 03:18:22
 
-SELECT CITY, STATE
-FROM STATION
+SELECT city, state
+from station
