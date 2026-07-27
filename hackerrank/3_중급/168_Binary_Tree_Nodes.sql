fe2002633b2cec650a60dcc6385b708a41539466
Hayeon627 <hy2000627@gmail.com>
2026-07-28 03:22:45 +0900

    [HackerRank] 중급 - Binary Tree Nodes (#168)

---
+++ hackerrank/3_중급/168_Binary_Tree_Nodes.sql
@@ -0,0 +1,14 @@
+-- Binary Tree Nodes
+-- HackerRank 중급 (⭐⭐⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/binary-search-tree-1/problem
+-- 작성자: 김하연
+-- 작성일: 2026. 07. 28. 03:22:44
+
+SELECT N,
+       CASE 
+           WHEN P IS NULL THEN 'Root'
+           WHEN N IN (SELECT DISTINCT P FROM BST WHERE P IS NOT NULL) THEN 'Inner'
+           ELSE 'Leaf'
+       END
+FROM BST
+ORDER BY N
