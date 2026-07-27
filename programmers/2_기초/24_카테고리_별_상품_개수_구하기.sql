c899bcb8de219b5f70a332efeedb7e01981369e0
Hayeon627 <hy2000627@gmail.com>
2026-07-28 03:47:48 +0900

    [프로그래머스] 기초 - 카테고리 별 상품 개수 구하기 (#24)

---
+++ programmers/2_기초/24_카테고리_별_상품_개수_구하기.sql
@@ -0,0 +1,11 @@
+-- 카테고리 별 상품 개수 구하기
+-- 프로그래머스 기초 (⭐⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131529
+-- 작성자: 김하연
+-- 작성일: 2026. 07. 28. 03:47:43
+
+-- 코드를 입력하세요
+SELECT SUBSTR(PRODUCT_CODE,1,2) AS CATEGORY, COUNT(*) AS PRODUCTS
+FROM PRODUCT
+GROUP BY SUBSTR(PRODUCT_CODE,1,2)
+ORDER BY CATEGORY
