85c7720a132251284d72ba0154224a8d669a7198
Hayeon627 <hy2000627@gmail.com>
2026-07-28 03:42:59 +0900

    [프로그래머스] 입문 - 중복 제거하기 (#3)

---
+++ programmers/1_입문/3_중복_제거하기.sql
@@ -0,0 +1,9 @@
+-- 중복 제거하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59408
+-- 작성자: 김하연
+-- 작성일: 2026. 07. 28. 03:42:55
+
+-- 코드를 입력하세요
+SELECT COUNT(DISTINCT NAME) AS COUNT
+FROM ANIMAL_INS
