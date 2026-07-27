a338d46c078444edcc987f5fe44e941fcc1d4196
Hayeon627 <hy2000627@gmail.com>
2026-07-28 03:58:00 +0900

    [프로그래머스] 기초 - 성분으로 구분한 아이스크림 총 주문량 (#39)

---
+++ programmers/2_기초/39_성분으로_구분한_아이스크림_총_주문량.sql
@@ -0,0 +1,13 @@
+-- 성분으로 구분한 아이스크림 총 주문량
+-- 프로그래머스 기초 (⭐⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/133026
+-- 작성자: 김하연
+-- 작성일: 2026. 07. 28. 03:57:55
+
+-- 코드를 입력하세요
+SELECT I.INGREDIENT_TYPE, 
+    SUM(F.TOTAL_ORDER) AS TOTAL_ORDER
+FROM FIRST_HALF F
+JOIN ICECREAM_INFO I
+ON F.FLAVOR = I.FLAVOR
+GROUP BY I.INGREDIENT_TYPE
