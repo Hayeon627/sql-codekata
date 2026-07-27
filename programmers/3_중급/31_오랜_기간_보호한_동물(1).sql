f59508c16396586288f6391031c723bb6597c52c
Hayeon627 <hy2000627@gmail.com>
2026-07-28 03:57:07 +0900

    [프로그래머스] 중급 - 오랜 기간 보호한 동물(1) (#31)

---
+++ programmers/3_중급/31_오랜_기간_보호한_동물(1).sql
@@ -0,0 +1,14 @@
+-- 오랜 기간 보호한 동물(1)
+-- 프로그래머스 중급 (⭐⭐⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59044
+-- 작성자: 김하연
+-- 작성일: 2026. 07. 28. 03:57:02
+
+-- 코드를 입력하세요
+SELECT I.NAME, I.DATETIME
+FROM ANIMAL_INS I
+LEFT JOIN ANIMAL_OUTS O
+ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE O.ANIMAL_ID IS NULL
+ORDER BY I.DATETIME
+LIMIT 3
