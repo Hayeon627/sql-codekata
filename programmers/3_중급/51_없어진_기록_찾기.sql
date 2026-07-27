9630f83b15ea74b281eb895ca3a24405e2522060
Hayeon627 <hy2000627@gmail.com>
2026-07-28 03:57:43 +0900

    [프로그래머스] 중급 - 없어진 기록 찾기 (#51)

---
+++ programmers/3_중급/51_없어진_기록_찾기.sql
@@ -0,0 +1,13 @@
+-- 없어진 기록 찾기
+-- 프로그래머스 중급 (⭐⭐⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59042
+-- 작성자: 김하연
+-- 작성일: 2026. 07. 28. 03:57:39
+
+-- 코드를 입력하세요
+SELECT O.ANIMAL_ID, O.NAME
+FROM ANIMAL_OUTS O
+LEFT JOIN ANIMAL_INS I
+ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.ANIMAL_ID IS NULL
+ORDER BY O.ANIMAL_ID
