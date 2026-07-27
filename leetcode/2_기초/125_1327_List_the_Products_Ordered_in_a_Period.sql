6a6d3d2e9605cc2e86d896c69414dc39b647e280
Hayeon627 <hy2000627@gmail.com>
2026-07-28 03:13:04 +0900

    [LeetCode] 기초 - 1327. List the Products Ordered in a Period (#125)

--- leetcode/2_기초/125_1327_List_the_Products_Ordered_in_a_Period.sql
+++ leetcode/2_기초/125_1327_List_the_Products_Ordered_in_a_Period.sql
@@ -2,14 +2,17 @@
 -- LeetCode 기초 (⭐⭐)
 -- 문제 링크: https://leetcode.com/problems/list-the-products-ordered-in-a-period/
 -- 작성자: 김하연
--- 작성일: 2026. 07. 28. 03:11:41
+-- 작성일: 2026. 07. 28. 03:13:03
 
+# Write your MySQL query statement below
+select product_name, unit
+from (
+    select p.product_id, p.product_name, p.product_category, o.order_date, sum(o.unit) as unit
+    from products p join orders o on p.product_id = o.product_id
+    and o.order_date like '2020-02%'
+    group by p.product_id
 ) t
 where unit >= 100
 
 -- SELECT p.product_name, SUM(o.unit) AS unit
 -- FROM Products p 
--- JOIN Orders o ON p.product_id = o.product_id
--- WHERE o.order_date >= '2020-02-01' AND o.order_date <= '2020-02-29'
--- GROUP BY p.product_id, p.product_name
--- HAVING SUM(o.unit) >= 100;
