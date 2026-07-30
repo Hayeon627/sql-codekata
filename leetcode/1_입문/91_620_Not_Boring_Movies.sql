9e2326bbfbce05b0c6b429cb5bd4091f60206cfd
Hayeon627 <hy2000627@gmail.com>
2026-07-31 01:00:30 +0900

    [LeetCode] 입문 - 620. Not Boring Movies (#91)

---
+++ leetcode/1_입문/91_620_Not_Boring_Movies.sql
@@ -0,0 +1,11 @@
+-- 620. Not Boring Movies
+-- LeetCode 입문 (⭐)
+-- 문제 링크: https://leetcode.com/problems/not-boring-movies/
+-- 작성자: 김하연
+-- 작성일: 2026. 07. 31. 01:00:29
+
+# Write your MySQL query statement below
+select *
+from Cinema
+where id % 2 != 0 and description not like '%boring%'
+order by rating desc
