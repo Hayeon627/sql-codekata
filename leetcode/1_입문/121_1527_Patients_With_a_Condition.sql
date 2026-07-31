484ce256e57d93b72535248ef413cb856f4b2356
Hayeon627 <hy2000627@gmail.com>
2026-07-31 18:10:21 +0900

    [LeetCode] 입문 - 1527. Patients With a Condition (#121)

--- leetcode/1_입문/121_1527_Patients_With_a_Condition.sql
+++ leetcode/1_입문/121_1527_Patients_With_a_Condition.sql
@@ -2,9 +2,10 @@
 -- LeetCode 입문 (⭐)
 -- 문제 링크: https://leetcode.com/problems/patients-with-a-condition/
 -- 작성자: 김하연
--- 작성일: 2026. 07. 31. 18:09:34
+-- 작성일: 2026. 07. 31. 18:10:20
 
 # Write your MySQL query statement below
 select *
 from Patients
-where conditions like '%DIAB1%'
+where conditions like 'DIAB1%'
+or conditions like '% DIAB1%'
