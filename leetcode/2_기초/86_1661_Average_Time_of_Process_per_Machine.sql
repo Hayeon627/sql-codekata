d2bf2bb03fcab88bb5b6bb4aee2d60f46965e215
Hayeon627 <hy2000627@gmail.com>
2026-08-01 17:49:55 +0900

    [LeetCode] 기초 - 1661. Average Time of Process per Machine (#86)

--- leetcode/2_기초/86_1661_Average_Time_of_Process_per_Machine.sql
+++ leetcode/2_기초/86_1661_Average_Time_of_Process_per_Machine.sql
@@ -2,17 +2,17 @@
 -- LeetCode 기초 (⭐⭐)
 -- 문제 링크: https://leetcode.com/problems/average-time-of-process-per-machine/
 -- 작성자: 김하연
--- 작성일: 2026. 08. 01. 17:49:02
+-- 작성일: 2026. 08. 01. 17:49:54
+
+
+group by machine_id
+from machine
+select machine_id, round(avg(time), 3) as processing_time
 
 )
-PIVOT (
-    MAX(timestamp) 
-    FOR activity_type IN ('start' AS start_time, 'end' AS end_time)
-)
-GROUP BY machine_id;
-    FROM Activity
-    SELECT machine_id, process_id, activity_type, timestamp
-FROM (
-    ROUND(AVG(end_time - start_time), 3) AS processing_time
-    machine_id, 
-SELECT 
+group by machine_id, process_id
+from Activity
+               end) as time
+                      when activity_type = 'end' then timestamp
+        sum(case when activity_type = 'start' then -timestamp
+    select machine_id, process_id,
