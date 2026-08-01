1233215f2dcf3995d9f75b70ad2c5a9b7dfccd99
Hayeon627 <hy2000627@gmail.com>
2026-08-01 17:04:22 +0900

    [LeetCode] 기초 - 197. Rising Temperature (#85)

--- leetcode/2_기초/85_197_Rising_Temperature.sql
+++ leetcode/2_기초/85_197_Rising_Temperature.sql
@@ -2,12 +2,15 @@
 -- LeetCode 기초 (⭐⭐)
 -- 문제 링크: https://leetcode.com/problems/rising-temperature/
 -- 작성자: 김하연
--- 작성일: 2026. 08. 01. 17:02:01
+-- 작성일: 2026. 08. 01. 17:04:21
 
 # Write your MySQL query statement below
 select id
 from (
-    select id, recordDate, temperature, lag(temperature) over(order by recordDate) as lag_temperature
-) t
+    select id, recordDate, temperature,
+        lag(temperature) over(order by recordDate) as lag_temperature,
+        lag(recordDate) over(order by recordDate) as lag_date
     from Weather
+) t
 where temperature > lag_temperature
+and dateDiff(recordDate, lag_date) = 1
