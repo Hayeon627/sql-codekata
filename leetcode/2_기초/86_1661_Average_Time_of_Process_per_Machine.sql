2117c7c73924d890aaa287c7826aa9259f0e06f7
Hayeon627 <hy2000627@gmail.com>
2026-08-01 18:00:32 +0900

    [LeetCode] 기초 - 1661. Average Time of Process per Machine (#86)

--- leetcode/2_기초/86_1661_Average_Time_of_Process_per_Machine.sql
+++ leetcode/2_기초/86_1661_Average_Time_of_Process_per_Machine.sql
@@ -2,33 +2,33 @@
 -- LeetCode 기초 (⭐⭐)
 -- 문제 링크: https://leetcode.com/problems/average-time-of-process-per-machine/
 -- 작성자: 김하연
--- 작성일: 2026. 08. 01. 17:54:37
+-- 작성일: 2026. 08. 01. 18:00:31
 
---     FROM Activity
---     SELECT machine_id, process_id, activity_type, timestamp
--- FROM (
---     ROUND(AVG(end_time - start_time), 3) AS processing_time
---     machine_id, 
--- SELECT 
-# Oracle
-
-
-group by machine_id
-from machine
-select machine_id, round(avg(time), 3) as processing_time
--- )
--- PIVOT (
---     MAX(timestamp) 
---     FOR activity_type IN ('start' AS start_time, 'end' AS end_time)
--- )
--- GROUP BY machine_id;
-
+# Write your MySQL query statement below
+with machine as (
+    select machine_id, process_id,
+        sum(case when activity_type = 'start' then -timestamp
+                      when activity_type = 'end' then timestamp
+               end) as time
+from Activity
+group by machine_id, process_id
 )
-group by machine_id, process_id
-from Activity
-               end) as time
-                      when activity_type = 'end' then timestamp
-        sum(case when activity_type = 'start' then -timestamp
-    select machine_id, process_id,
-with machine as (
-# Write your MySQL query statement below
+
+select machine_id, round(avg(time), 3) as processing_time
+from machine
+group by machine_id
+
+
+# Oracle
+-- SELECT 
+--     machine_id, 
+--     ROUND(AVG(end_time - start_time), 3) AS processing_time
+-- FROM (
+--     SELECT machine_id, process_id, activity_type, timestamp
+--     FROM Activity
+-- )
+-- PIVOT (
+--     MAX(timestamp) 
+--     FOR activity_type IN ('start' AS start_time, 'end' AS end_time)
+-- )
+-- GROUP BY machine_id;
