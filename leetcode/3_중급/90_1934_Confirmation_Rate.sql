a007b6baf870816a3c7fe8520c5ee62cebbe5fff
Hayeon627 <hy2000627@gmail.com>
2026-08-07 14:25:26 +0900

    [LeetCode] 중급 - 1934. Confirmation Rate (#90)

--- leetcode/3_중급/90_1934_Confirmation_Rate.sql
+++ leetcode/3_중급/90_1934_Confirmation_Rate.sql
@@ -2,7 +2,7 @@
 -- LeetCode 중급 (⭐⭐⭐)
 -- 문제 링크: https://leetcode.com/problems/confirmation-rate/
 -- 작성자: 김하연
--- 작성일: 2026. 08. 07. 14:25:18
+-- 작성일: 2026. 08. 07. 14:25:25
 
 # Write your MySQL query statement below
 select user_id, round(sum(confirmed)/count(*), 2) as confirmation_rate
@@ -11,7 +11,3 @@ from (
     from Signups s left join Confirmations c on s.user_id = c.user_id
 ) t
 group by 1
-
-
--- select s.user_id, c.action, (case when c.action = 'confirmed' then 1 else 0 end) as confirmed
--- from Signups s left join Confirmations c on s.user_id = c.user_id
