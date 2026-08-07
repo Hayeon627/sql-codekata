137d65810b29c0c20cb09d83607f6e718a440453
Hayeon627 <hy2000627@gmail.com>
2026-08-07 14:12:45 +0900

    [LeetCode] 중급 - 1280. Students and Examinations (#88)

---
+++ leetcode/3_중급/88_1280_Students_and_Examinations.sql
@@ -0,0 +1,14 @@
+-- 1280. Students and Examinations
+-- LeetCode 중급 (⭐⭐⭐)
+-- 문제 링크: https://leetcode.com/problems/students-and-examinations/
+-- 작성자: 김하연
+-- 작성일: 2026. 08. 07. 14:12:44
+
+# Write your MySQL query statement below
+select st.student_id, st.student_name, sb.subject_name, count(ex.subject_name) as attended_exams
+from Students st
+cross join Subjects sb
+left join Examinations ex on st.student_id = ex.student_id
+and ex.subject_name = sb.subject_name
+group by st.student_id, sb.subject_name
+order by 1, 3
